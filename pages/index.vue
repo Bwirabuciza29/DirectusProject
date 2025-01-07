b929c4efd832d84913cdd21318589ea060ac149c
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2025-01-07 13:56:35 +0200

    HereWeGo

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,38 @@
 <template>
-  <form
-    @submit.prevent="submitSubscription"
-    class="max-w-md mx-auto p-6 bg-white shadow-md rounded-lg"
-  >
-    <div class="mb-4">
-      <label for="email" class="block text-sm font-medium text-gray-700">
-        Adresse email
-      </label>
-      <input
-        v-model="email"
-        id="email"
-        type="email"
-        placeholder="Entrez votre email"
-        required
-        class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500"
-      />
-    </div>
-
-    <button
-      class="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-200"
+  <div>
+    <Nuxtdirectus />
+    <form
+      @submit.prevent="submitSubscription"
+      class="max-w-md mx-auto p-6 bg-white shadow-md rounded-lg"
     >
-      Souscrire
-    </button>
+      <div class="mb-4">
+        <label for="email" class="block text-sm font-medium text-gray-700">
+          Adresse email
+        </label>
+        <input
+          v-model="email"
+          id="email"
+          type="email"
+          placeholder="Entrez votre email"
+          required
+          class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500"
+        />
+      </div>
 
-    <p v-if="successMessage" class="mt-4 text-green-600 font-medium">
-      {{ successMessage }}
-    </p>
-    <p v-if="errorMessage" class="mt-4 text-red-600 font-medium">
-      {{ errorMessage }}
-    </p>
-  </form>
+      <button
+        class="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-200"
+      >
+        Souscrire
+      </button>
+
+      <p v-if="successMessage" class="mt-4 text-green-600 font-medium">
+        {{ successMessage }}
+      </p>
+      <p v-if="errorMessage" class="mt-4 text-red-600 font-medium">
+        {{ errorMessage }}
+      </p>
+    </form>
+  </div>
 </template>
 
 <script setup>
